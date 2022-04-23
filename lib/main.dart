04d3076e2a76aa05f6251e861d616eba9b3d6528
ideshunta <shuntaide16213@gmail.com>
2022-04-23 21:52:05 +0900

    delete unused package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'package:kaihatsu_test/login_form.dart';
