8dd472a0f9665b7bd03977aa61b2eb7221639cfe
ideshunta <shuntaide16213@gmail.com>
2022-05-05 02:38:02 +0900

    ユーザー名登録を追加

--- lib/model/authenticationData/authenticationData.dart
+++ lib/model/authenticationData/authenticationData.dart
@@ -1,25 +1,38 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/// Authenticationに使うProviderまとめ
+
+/// email の状態管理用　
 final emailProvider = StateProvider<String>((ref) {
   return '';
 });
 
+/// password の状態管理用
 final passwordProvider = StateProvider<String>((ref) {
   return '';
 });
 
+/// 確認用パスワード　の状態管理用
 final passwordCheckProvider = StateProvider<String>((ref) {
   return '';
 });
 
+/// サインイン時のエラーテキストの状態管理用
 final errorTextProvider = StateProvider<String>((ref) {
   return '';
 });
 
+/// パスワードの表示非表示切り替え用
 final passwordVeilProvider = StateProvider.autoDispose<bool>((ref) {
   return true;
 });
 
+/// SignUp/SignIn時のinfoTextの状態管理用
 final signUpTextProvider = StateProvider<String>((ref) {
+  return '';
+});
+
+/// ユーザー名の状態管理用
+final userNameProvider = StateProvider<String>((ref) {
   return '';
 });
