885f3d7a02ac1a59824d2621289061ca4f8efd40
ideshunta <shuntaide16213@gmail.com>
2022-05-07 00:49:48 +0900

    manual Merge pull request #22 by hinano and add small fix by shunta

--- lib/view/pages/addDishes.dart
+++ lib/view/pages/addDishes.dart
@@ -1,43 +1,46 @@
 import 'package:flutter/material.dart';
 
-
 class AddDishes extends StatefulWidget {
-  const AddDishes({Key? key}) :super(key: key);
+  const AddDishes({Key? key}) : super(key: key);
 
   @override
   State<AddDishes> createState() => _AddDishesState();
 }
 
 class _AddDishesState extends State<AddDishes> {
-  String? isSelectedItem = '鶏肉';
+  String? isSelectedItem = '';
+  String? message = '';
+  String? food = '';
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DropdownButton Sample'),
-      ),
+      //appBar: AppBar(
+      //title: const Text('DropdownButton Sample'),
+      //),
       body: Center(
         child: Container(
           decoration: BoxDecoration(
-            color: Colors.transparent,
-            borderRadius: BorderRadius.circular(40)
-          ),
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(15)),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
+              const Text(
+                'ジャンル',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
               Container(
-                margin: EdgeInsets.only(top: 30,bottom: 3),
+                margin: const EdgeInsets.only(top: 0, bottom: 30),
                 width: 200,
-                height: 150,
-                child:DropdownButtonFormField(
-                  decoration:  InputDecoration(
+                height: 70,
+                child: DropdownButtonFormField(
+                  decoration: const InputDecoration(
                     enabledBorder: OutlineInputBorder(
-                      borderRadius: const BorderRadius.all(
-                          const Radius.circular(30.0),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30.0),
                       ),
                       borderSide: BorderSide(
                         color: Colors.orange,
@@ -47,20 +50,24 @@ class _AddDishesState extends State<AddDishes> {
 
                   items: const [
                     DropdownMenuItem(
-                      child: Text('鶏肉'),
-                      value: '鶏肉',
+                      child: Text('牛肉'),
+                      value: '牛肉',
                     ),
                     DropdownMenuItem(
                       child: Text('豚肉'),
                       value: '豚肉',
                     ),
                     DropdownMenuItem(
-                      child: Text('牛肉'),
-                      value: '牛肉',
+                      child: Text('鶏肉'),
+                      value: '鶏肉',
                     ),
                     DropdownMenuItem(
                       child: Text('魚類'),
                       value: '魚類',
+                    ),
+                    DropdownMenuItem(
+                      child: Text('副菜'),
+                      value: '副菜',
                     ),
                     DropdownMenuItem(
                       child: Text('麺類'),
@@ -71,16 +78,12 @@ class _AddDishesState extends State<AddDishes> {
                       value: '汁物',
                     ),
                     DropdownMenuItem(
-                      child: Text('副菜'),
-                      value: '副菜',
-                    ),
-                    DropdownMenuItem(
                       child: Text('その他'),
                       value: 'その他',
                     ),
                   ],
 
-                //6
+                  //6
                   onChanged: (String? value) {
                     setState(() {
                       isSelectedItem = value;
@@ -90,60 +93,68 @@ class _AddDishesState extends State<AddDishes> {
                   value: isSelectedItem,
                 ),
               ),
-
-
-
-              Container(
-                margin: EdgeInsets.only(top:0, bottom: 10),
-                width:200,
-                height:50,
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide(
-                      color: Colors.amber,
-                      )
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide(
-                      color: Colors.amber,
-                      )
-                    ),
-                  ),
+              const Text(
+                'メニュー名',
+                style: TextStyle(
+                  fontSize: 20,
                 ),
               ),
-
-
-
               Container(
-                width:300,
-                height:100,
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.message,
-                      color: Colors.black,
+                margin: const EdgeInsets.only(top: 0, bottom: 30),
+                width: 200,
+                height: 60,
+                child: TextFormField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: const BorderSide(
+                            color: Colors.orange,
+                          )),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: const BorderSide(
+                            color: Colors.orange,
+                          )),
                     ),
-                    border:
-                      OutlineInputBorder(),
-                  ),
-                )
+                    onChanged: (word) {
+                      food = word;
+                    }),
               ),
-
-              Container(
-                child: Material(
-                  color: Colors.lightGreen,
-                  shape: CircleBorder(
-                    side: BorderSide(),
-                  ),
-                  child: Icon(
-                      Icons.check,
-                      color: Colors.white,
-                      size: 50,
-                  ),
+              const Text(
+                'メモ',
+                style: TextStyle(
+                  fontSize: 20,
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(40),
+                child: TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 6,
+                    minLines: 3,
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.all(30),
+                      prefixIcon: Icon(
+                        Icons.message,
+                        color: Colors.black,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.orange,
+                        ),
+                      ),
+                    ),
+                    onChanged: (text) {
+                      message = text;
+                    }),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                child: const Icon(Icons.check),
+                padding: const EdgeInsets.all(16),
+                color: Colors.orange,
+                textColor: Colors.white,
+                shape: const CircleBorder(),
               ),
             ],
           ),
@@ -155,9 +166,8 @@ class _AddDishesState extends State<AddDishes> {
         padding: const EdgeInsets.only(bottom: 10.0),
         child: FloatingActionButton(
           child: const Icon(Icons.clear),
-          onPressed: () => Navigator.of(context).pop() ,
+          onPressed: () => Navigator.of(context).pop(),
         ),
-
       ),
     );
   }
