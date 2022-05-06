4467d3aa802c9f32c118bdf17d8212ea9f387505
ideshunta <shuntaide16213@gmail.com>
2022-05-07 00:31:55 +0900

    manual Merge pull request #23

---
+++ lib/const/dishList/popupButtonItem.dart
@@ -0,0 +1,5 @@
+enum popupMenuItem {
+  none,
+  update,
+  delete,
+}
--- lib/view/dishList/updateDish.dart
+++ lib/view/dishList/updateDish.dart
@@ -1,24 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void deleteDish({
+void updateDish({
   required BuildContext context,
   required String uid,
   required DocumentSnapshot document,
+  required DateTime now,
 }) async {
   await showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('消去確認'),
-          content: Text('${document.get('name')}を消去しますか？'),
+          title: const Text('作成日更新'),
+          content: Text('${document.get('name')}の作成日を変更しますか？'),
           actions: <Widget>[
             ElevatedButton(
               onPressed: () async {
                 await FirebaseFirestore.instance
                     .collection(uid)
                     .doc(document.get('name'))
-                    .delete();
+                    .update({
+                  'date': now,
+                });
                 Navigator.of(context).pop();
               },
               child: Text('確認'),
