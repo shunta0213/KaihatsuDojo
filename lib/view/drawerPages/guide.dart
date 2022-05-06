a07009c9358e7c0665edc973f1cc07ccd0bbeebf
asuka1211w <wwwaaa_317@icloud.com>
2022-05-06 22:39:00 +0900

    使い方ガイド、途中まで作りました。

--- lib/view/drawerPages/guide.dart
+++ lib/view/drawerPages/guide.dart
@@ -7,7 +7,35 @@ class Guide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('このアプリの使い方', style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+           child: Column(
+             children: [Container(
+             width: double.infinity,
+             child:
+             Text('料理名を追加する時',style: TextStyle(fontSize: 20,color:Colors.blueAccent,decoration: TextDecoration.underline))),
+               Container(color: Colors.white,width: double.infinity,
+                   child:const Text(
+                     "画面中央の下にある、オレンジの＋ボタンを押します。"),),
+               Image.asset('assets/images/appIcon.png'),
+               Text(  "次に、８つのカテゴリの中から追加したい料理が当てはまるカテゴリを選びます。"),
+               Image.asset('assets/images/appIcon.png'),
+               Text("入力が終わったら✅ボタンを押し、料理の追加は完了です。"),
+               Image.asset('assets/images/appIcon.png'),
+               myContainer(color: Colors.white),
+               myContainer(color: Colors.white),
+               const Text('料理名を選択する時',style: TextStyle(fontSize: 25,decoration: TextDecoration.underline)),
+               myContainer(color: Colors.white),
+               myContainer(color: Colors.white),
+               myContainer(color: Colors.white),]
+               ),
+           ),
+         ),
       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.clear),
@@ -15,6 +43,29 @@ class Guide extends StatelessWidget {
           Navigator.of(context).pop();
         },
       ),
-    );
-  }
+      );
+    }
 }
+    Widget myContainer( { double size = 300,required Color color, String text = ''}) {
+      return Container(
+        color: color,
+        height: size,
+
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+            ),
+          ),
+        ),
+      );
+    }
+
+
+
+
+
+
