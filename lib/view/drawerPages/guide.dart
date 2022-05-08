288b4e0b3ac30065c81783a6e424eae8c5265e5d
asuka1211w <wwwaaa_317@icloud.com>
2022-05-08 14:05:45 +0900

    使い方ガイド出来ました！

--- lib/view/drawerPages/guide.dart
+++ lib/view/drawerPages/guide.dart
@@ -15,7 +15,7 @@ class Guide extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
            child: Column(
-             children: [Text('このアプリは、自分が作った料理を追加していくことで、後からどんな料理を作ったか見返せるアプリです。\n\n'),
+             children: [Text('このアプリは、自分が作った料理を追加していくことで、後からどんな料理を作ったかを見返せるアプリです。\n\n'),
                Container(
              width: double.infinity,
              child:
@@ -24,42 +24,48 @@ class Guide extends StatelessWidget {
                    child:const Text(
                      "画面中央の下にある、オレンジの＋ボタンを押します"),),
                Image.asset('assets/images/button.JPEG'),
-               Text(  "次に、８つのカテゴリの中から追加したい料理が当てはまるカテゴリを選びます。"),
-               Image.asset('assets/images/appIcon.png'),
-               Text("入力が終わったら✅ボタンを押し、料理の追加は完了です。"),
-               Image.asset('assets/images/appIcon.png'),
+               Text(  "次に、追加したい料理が当てはまるジャンルを選び、メニュー名を入力し、その料理に関するメモがあればメモを残します。"),
+               Image.asset('assets/images/ジャンル.JPEG'),
+               Text("入力が終わったら✅ボタンを押し、料理の追加は完了です。\n"),
+               Image.asset('assets/images/checkbutton2.JPEG'),
                Container(
                    width: double.infinity,
                    child:Text('料理名を選択する時', textAlign: TextAlign.left,style: TextStyle(fontSize: 20,color:Colors.blueAccent,decoration: TextDecoration.underline))),
-               Image.asset('assets/images/appIcon.png'),
+          Container(
+              width: double.infinity,
+              child:Text('まず、画面右下のlistボタンを押します。')),
+               Image.asset('assets/images/list.JPEG'),
+              Text('\n\n次に、その料理のジャンルを、以下の８個の中から選びます。'),
+               Image.asset('assets/images/選択.JPEG'),
+               Text('すると、以前自分が追加していた料理が出て来るため、料理の選択ができます。\n\n'),
           Container(
               width: double.infinity,
               child:Text('もっと活用するために', textAlign: TextAlign.left,style: TextStyle(fontSize: 20,color:Colors.blueAccent,decoration: TextDecoration.underline)),),
-                Text('日付が表示されている画面の、左上の三本線のボタンを押してみてください。'),
+                Text('日付が表示されている画面の、左上の三本線のボタンを押してください。'),
+               Image.asset('assets/images/三本線2.JPEG'),
                Container(
               width: double.infinity,
-              child:Text('・メモ機能を使ってみましょう',style: TextStyle(color:Colors.lightBlueAccent,fontSize:18))),
-
-               Image.asset('assets/images/appIcon.png'),
+              child:Text('・メモを使ってみましょう\n',style: TextStyle(color:Colors.lightBlueAccent,fontSize:18))),
           Container(
               width: double.infinity,
-              child:Text('使い方の例\n',style: TextStyle(decoration: TextDecoration.underline,fontSize: 16))),
+              child:Text('メモを押します。')),
+               Image.asset('assets/images/メモ2.JPEG'),
+          Container(
+              width: double.infinity,
+              child:Text('\n使い方の例\n',style: TextStyle(decoration: TextDecoration.underline,fontSize: 16))),
                    Text('・賞味期限が近い食材を入力しておいて、外出先で見る事ができるようにする\n'
                    '・食べたい物リストを作り、献立時に参考にする\n\n'
                    '',textAlign: TextAlign.left),
                Text('この他にも、自分だけのメモ機能の使い方を見つけてみましょう。\n\n\n'),
               Container(
                width: double.infinity,
-               child:Text('・ぶたのちょきんばこ機能を使ってみましょう',style: TextStyle(color:Colors.lightBlueAccent,fontSize:18))),
-               Image.asset('assets/images/appIcon.png'),
-               Container(
-               width: double.infinity,
-               child:Text('使い方の例\n')),
+               child:Text('・検索や、電子書籍、記事を使ってみましょう',style: TextStyle(color:Colors.lightBlueAccent,fontSize:18))),
+               Text('ボタンを押す事で、それぞれのリンクの画面に飛ぶ事ができます。'),
+               Image.asset('assets/images/検索.JPEG'),
           Container(
               width: double.infinity,
-              child:Text('・検索機能を使ってみましょう',style: TextStyle(color:Colors.lightBlueAccent,fontSize:18))),
-               Text('ボタンを押す事で、それぞれのリンクの検索画面に飛ぶ事ができます。'),
-               Container(height:200)
+              child:Text('\n料理を作るときに参考にしてみてください。')),
+          Container(height: 100),
              ]
                ),
            ),
